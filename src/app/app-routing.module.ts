9b08aa01d443ae957be6785cb4282c5dfa224c72
Aneta Marttila <anmart@ttu.ee>
2021-04-30 16:00:16 +0300

    edited routing - now applications default "landing page" is search form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import {WordFormComponent} from './word-form/word-form.component';
 import {WordSearchFormComponent} from './word-search-form/word-search-form.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'search-word', pathMatch: 'full' },
   {path: 'add-word', component: WordFormComponent},
   {path: 'search-word', component: WordSearchFormComponent}
 ];
