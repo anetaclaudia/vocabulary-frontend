4ffbf4eaaceef618b5c70ec46ec782dad6b60aec
Aneta Marttila <anmart@ttu.ee>
2021-05-05 15:04:00 +0300

    added validations to word-form

--- src/app/word-form/word-form.component.ts
+++ src/app/word-form/word-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AbstractControl, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {WordService} from '../service/word.service';
 import {Word} from '../model/Word';
@@ -11,24 +11,42 @@ import {Word} from '../model/Word';
 })
 export class WordFormComponent implements OnInit {
   wordForm: FormGroup;
+
   constructor(private formBuilder: FormBuilder,
-              private wordService: WordService) { }
+              private wordService: WordService) {
+  }
 
   ngOnInit(): void {
     this.wordForm = new FormGroup({
-      wordInEstonian: new FormControl('', [Validators.required]),
-      wordDefinitionInEstonian: new FormControl('', [Validators.required]),
-      wordInEnglish: new FormControl('', [Validators.required]),
-      wordDefinitionInEnglish: new FormControl('', [Validators.required])
+        wordInEstonian: new FormControl('', [Validators.required]),
+        wordDefinitionInEstonian: new FormControl('', [Validators.required]),
+        wordInEnglish: new FormControl('', [Validators.required]),
+        wordDefinitionInEnglish: new FormControl('', [Validators.required])
       }
     );
   }
 
-  onSubmit(): void{
+  onSubmit(): void {
     const word: Word = this.wordForm.value as Word;
     console.log(word);
     this.wordService.addWord(word).subscribe(_ => {
       console.log('subscribing');
     });
+  }
+
+  get wordInEstonian(): AbstractControl {
+    return this.wordForm.get('wordInEstonian');
+  }
+
+  get wordDefinitionInEstonian(): AbstractControl {
+    return this.wordForm.get('wordDefinitionInEstonian');
+  }
+
+  get wordInEnglish(): AbstractControl {
+    return this.wordForm.get('wordInEnglish');
+  }
+
+  get wordDefinitionInEnglish(): AbstractControl {
+    return this.wordForm.get('wordDefinitionInEnglish');
   }
 }
