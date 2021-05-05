788b394084cd60a5a40a2289357029034f44198e
Aneta Marttila <anmart@ttu.ee>
2021-05-05 15:10:48 +0300

    cleaned up code and added navbar brand

--- src/app/word-search-form/word-search-form.component.ts
+++ src/app/word-search-form/word-search-form.component.ts
@@ -3,7 +3,6 @@ import {Language} from '../model/Language';
 import {FormControl, FormGroup, Validators, ReactiveFormsModule} from '@angular/forms';
 import {WordService} from '../service/word.service';
 import {Word} from '../model/Word';
-import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-word-search-form',
@@ -12,19 +11,16 @@ import {Observable} from 'rxjs';
 })
 export class WordSearchFormComponent implements OnInit {
   public words: Word[];
-  // page = 1;
-  // pageSize = 4;
-  // collectionSize = words.length;
 
   languages = Object.values(Language);
 
   searchForm = new FormGroup({
     searchWord: new FormControl('', [Validators.required]),
-    language: new FormControl(null, [Validators.required])
+    language: new FormControl(null)
   });
 
   constructor(private wordService: WordService) {
-    // this.refreshWords();
+
   }
 
   ngOnInit(): void {
@@ -36,10 +32,4 @@ export class WordSearchFormComponent implements OnInit {
       this.words = words;
     });
   }
-
-  // refreshWords(): void {
-  //   this.words = words
-  //     .map((word, i) => ({id: i + 1, ...word}))
-  //     .slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize);
-  // }
 }
